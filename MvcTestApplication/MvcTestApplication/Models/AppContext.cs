7ecbb448776daf456b0db7ceb46dd347c0533e11
Glareone <enlarius@gmail.com>
2013-07-11 23:26:46 +0300

    еще классы БД

---
+++ MvcTestApplication/MvcTestApplication/Models/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MvcTestApplication.Models;
+
+//    public class AppContext : DbContext<br />    {<br />       
+ //       public DbSet<Vacancy> Vacancies { get; set; }<br />      
+ //       public DbSet<Employer> Employers { get; set; }<br />    }<br />}
+//Н
+
+
+namespace MvcTestApplication.Models
+{
+    public class AppContext
+    {
+        public DbSet<UserID> UserID { get; set; }
+        public DbSet<RightGroups> RightGroups { get; set; }
+    }
+}
---
+++ MvcTestApplication/MvcTestApplication/Models/UserID.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcTestApplication.Models
+{
+    public class UserID
+    {
+        public int ID { get; set; }
+        public int OldPassword { get; set; }
+        public int NewPassword { get; set; }
+        public int Email { get; set; }
+    }
+}

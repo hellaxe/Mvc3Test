d83b1c279a1e9513c59aa3c96fd2ec615b24dd77
lis-san <fermindirak@gmail.com>
2013-07-15 00:32:26 +0300

    2
    3

--- MvcTestApplication/MvcTestApplication/Models/Comment.cs
+++ MvcTestApplication/MvcTestApplication/Models/Comment.cs
@@ -10,6 +10,6 @@ namespace MvcTestApplication.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
-        public string[] Text { get; set; }
+        public string Text { get; set; }
     }
 }
